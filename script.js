f97c26f5a6ea824b71e4b40bc53c45de3d2a97f9
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-04-24 13:50:55 +0200

    Fix name

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const person = {
   name: 'Johan Lieuw-A-Peuw',
   job: 'Designer'
 }
+
+// console.log(person)
+
+function listObject(object) {
+  return `Hello ${object.name}`
+}
+
+console.log(listObject(person))
