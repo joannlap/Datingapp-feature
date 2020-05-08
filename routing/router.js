9dde358d086f34b32456c128762f2ba7e0c95e6f
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-05-08 17:27:22 +0200

    HW week1

--- routing/router.js
+++ routing/router.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-// const aboutPage = require('./routing/about.js')
-// const contactPage = require('./routing/contact.js')
-// const error = require('./routing/404error.js')
+//loads the different routes in the routing folder
+const aboutPage = require('./routing/about.js')
+const contactPage = require('./routing/contact.js')
+const error = require('./routing/404error.js')
 
+//aboutpage route
 router.get('/about', aboutPage)
+//contactpage route
 router.get('/contact', contactPage)
+//404error route
 router.get('/404error', error)
 
 module.exports = router;
