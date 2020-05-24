5b69612f79c20bb0c9ab88475f8457f6e5d68049
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-05-24 23:37:28 +0200

    Added index.js package-lock.json package.json public/client.js public/main.css views/index.hbs views/match-list.hbs views/match.hbs views/partials/footer.hbs views/profile.hbs.

--- index.js
+++ index.js
@@ -1,52 +1,89 @@
 // variables
-const hbs = require('hbs')
-const express = require('express') //inladen van express package
-const app = express() //opstarten van express applicatie
-const port = 3000 //adres van je webserver
-const yoMomma = require('yo-mamma').default //extern package
-const fatoe = yoMomma()
-const path = require('path')
+const
+  hbs = require('hbs')
+express = require('express') //inladen van express package
+app = express() //opstarten van express applicatie
+port = 3000 //adres van je webserver
+yoMomma = require('yo-mamma').default //extern package
+fatoe = yoMomma()
+bodyParser = require('body-parser');
+path = require('path')
 
-app.set('view engine', 'hbs')
-app.set('views', 'views') //checkt door de views map
+app
+  .set('view engine', 'hbs')
+  .set('views', 'views')
+  .use(express.static('public')) // gebruikt deze map (public) om html bestanden te serveren
+  .use(bodyParser.urlencoded({
+    extended: true
+  }));
 
-// gebruikt deze map (public) om html bestanden te serveren
-app.use(express.static('public'));
 hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
 // different routes of static pages
 // Hbs tranformeert hbs files naar .html en vertsuurd deze naar public folder
+
+const userData = [{
+    name: 'Aang',
+    age: 17,
+    profession: 'airbending'
+  },
+  {
+    name: 'Zuko',
+    age: 19,
+    profession: 'firebending'
+  },
+  {
+    name: 'Sokka',
+    age: 19,
+    profession: 'waterbending'
+  },
+]
+
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'home',
-    fatoe
+    users: userData
   });
 });
+
+app.get('/match-list', (req, res) => {
+  res.render('match-list', {
+    title: ' match-list',
+    users: userData
+  })
+})
 
 app.get('/match', (req, res) => {
   res.render('match', {
     title: 'match',
-    name: 'Zuko',
-    age: 20,
-    isName: true,
-    isAgeValid: false
+    users: userData[Math.floor(Math.random() * userData.length)]
   });
 });
+
+// res.render => naar pagina
+// res.redirect => naar route
+
+//als je liked wordt je doorgestuurd naar de match pagina, zodra je disliked blijf je op de index
+app.post('/match', (req, res) => {
+  if (req.body.like) {
+    res.render('match', {
+      title: 'match',
+      users: userData[Math.floor(Math.random() * userData.length)]
+    })
+    console.log(req.body.like)
+  } else if (req.body.dislike) {
+    res.redirect('/')
+    console.log(req.body.dislike)
+  }
+})
 
 
 //renders couple objects containing arrays to the profile.hbs
 app.get('/profile', (req, res) => {
   res.render('profile', {
     title: 'profile',
-    people: [
-      "Katara",
-      "Aang",
-      "Sokka",
-      "Zuko",
-      "Toph",
-    ],
     interests: [{
-        skills: ['firebending', 'earthbending', 'airbending']
+        skills: ['firebending', 'earthbending', 'airbending', 'waterbending']
       },
       {
         hobbies: ['violin', 'reading']
@@ -60,3 +97,5 @@ app.get('/profile', (req, res) => {
 app.listen(port, function () {
   return console.log(`app draait op port ${port}!!`)
 });
+
+// home > match > profile > match overzicht
