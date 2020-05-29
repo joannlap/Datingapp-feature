89412b061dec03dfeb5a0a58f36c9cc85b467f4f
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-05-29 15:23:55 +0200

    no more use of Multer middleware

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require('dotenv').config();
 const mongo = require('mongodb');
 
 const router = new express.Router();
-const multer = require('multer');
-const sharp = require('sharp');
+// const multer = require('multer');
+// const sharp = require('sharp');
 let db = null;
 let usersList = null;
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_URL}`;
@@ -23,76 +23,8 @@ app
 
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-// different routes of static pages
-// Hbs tranformeert hbs files naar .html en vertsuurd deze naar public folder
-
-const upload = multer({
-  limits: {
-    fileSize: 1000000,
-  },
-  // deze functie limiteert de file-type naar images alleen
-  imageFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Only image files allowed!'));
-    }
-    cb(null, true);
-  },
-});
-
-// Multer middleware die single file upload 'profile-pic' naar database
-app.use(
-  router.post('/test', upload.single('profile-pic'), async (req, res) => {
-    const photo = await sharp(req.file.buffer)
-      .resize({
-        width: 250,
-        height: 250
-      })
-      .png()
-      .toBuffer();
-    db.collection('users')
-      .insertOne({
-        pic: photo,
-      }, )
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    res.send('done');
-  })
-);
-
-// retrieve list of images
-// app.get('/images', async (req, res) => {
-//   try {
-//     const photo = await sharp(req.file.buffer);
-//     res.send(photo.data);
-//   } catch (err) {
-//     res.sendStatus(400);
-//   }
-// });
-
-// retrieve images by id
-// app.get('/images/:id', async (req, res) => {
-//   try {
-//     const photo = await sharp(req.file.buffer);
-//     const result = photo.get(req.params.id);
-
-//     if (!result) {
-//       res.sendStatus(404);
-//       return;
-//     }
-//     res.setHeader('Content-Type', result.mimetype);
-    // fs.createReadStream(path.join('public/img/components', result.filename)).pipe(res);
-//   } catch (err) {
-//     res.sendStatus(400);
-//   }
-// });
-// https://scotch.io/tutorials/express-file-uploads-with-multer
 
 // CONNECT TO DATABASE
-
 mongo.MongoClient.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -108,11 +40,9 @@ mongo.MongoClient.connect(url, {
 });
 
 // laadt de indexpagina
-
 app.get('/', async (req, res, next) => {
   try {
     const allUsers = await usersList.find({}).toArray();
-    // console.log(exclMe);
     res.render('index', {
       title: 'home',
       users: allUsers
@@ -123,34 +53,60 @@ app.get('/', async (req, res, next) => {
 });
 
 
-// res.render => naar pagina
-// res.redirect => naar route
-
+// match route
 app.post('/match', (req, res) => {
   res.render('match', {
     title: 'match',
   });
 });
 
-app.get('/match-list', (req, res) => {
-  res.render('match-list', {
-    title: ' match-list',
-  });
+// fotos ophalen uit de database
+router.get('/users', async (req, res) => {
+  try {
+    const matchedUser = await db.collection('users').findOne({
+      name: 'Collin'
+    });
+    console.log(matchedUser);
+    if (!matchedUser || !matchedUser.img) {
+      throw new Error();
+    }
+    res.set('Content-Type', 'image/png');
+    res.status(200).send(matchedUser.img);
+    // stuurt een 404 error als de foto niet bestaat in de database
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
+router.get('/user', async (req, res) => {
+  try {
+    const user = await db.collection('users')
+      .findOne({
+        id: 1
+      });
+    console.log(user);
 
-// renders couple objects containing arrays to the profile.hbs
-app.get('/profile', (req, res) => {
-  res.render('profile', {
-    title: 'profile',
-    interests: [{
-        skills: ['firebending', 'earthbending', 'airbending', 'waterbending']
-      },
-      {
-        hobbies: ['violin', 'reading']
-      }
-    ]
-  });
+    res.status(200).render('profile', {
+      user
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/profile', async (req, res) => {
+  try {
+    const userOne = await usersList.find({
+      name: 'Collin'
+    }).toArray();
+    res.render('profile', {
+      title: 'profile',
+      users: userOne
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.get('/*', (req, res) => {
