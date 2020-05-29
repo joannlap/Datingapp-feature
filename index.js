2b86de94ba6879c65d6d0dab7efdee575f1c0245
joannlap <*jo-ann.lieuw-a-peuw@hva.nl*>
2020-05-29 21:55:19 +0200

    Added index.js public/main.css views/index.hbs views/match.hbs views/profile.hbs.

--- index.js
+++ index.js
@@ -39,66 +39,51 @@ mongo.MongoClient.connect(url, {
   usersList = db.collection('users');
 });
 
-// laadt de indexpagina
+// renders home page
 app.get('/', async (req, res, next) => {
   try {
-    const allUsers = await usersList.find({}).toArray();
+    const userOne = await usersList.find({}).toArray();
     res.render('index', {
       title: 'home',
-      users: allUsers
+      users: userOne
     });
   } catch (err) {
     next(err);
   }
 });
 
+// redirects to home
+app.post('/', async (req, res, next) => {
+  try {
+    const userOne = await usersList.find({}).toArray();
+    res.render('index', {
+      title: 'home',
+      users: userOne
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 // match route
-app.post('/match', (req, res) => {
-  res.render('match', {
-    title: 'match',
-  });
-});
-
-// fotos ophalen uit de database
-router.get('/users', async (req, res) => {
-  try {
-    const matchedUser = await db.collection('users').findOne({
-      name: 'Collin'
-    });
-    console.log(matchedUser);
-    if (!matchedUser || !matchedUser.img) {
-      throw new Error();
-    }
-    res.set('Content-Type', 'image/png');
-    res.status(200).send(matchedUser.img);
-    // stuurt een 404 error als de foto niet bestaat in de database
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-router.get('/user', async (req, res) => {
-  try {
-    const user = await db.collection('users')
-      .findOne({
-        id: 1
-      });
-    console.log(user);
-
-    res.status(200).render('profile', {
-      user
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.get('/profile', async (req, res) => {
+app.post('/match', async (req, res, next) => {
   try {
     const userOne = await usersList.find({
-      name: 'Collin'
+      name: 'Dré'
+    }).toArray();
+    res.render('match', {
+      title: 'match',
+      users: userOne
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/profile', async (req, res) => {
+  try {
+    const userOne = await usersList.find({
+      name: 'Dré'
     }).toArray();
     res.render('profile', {
       title: 'profile',
@@ -108,6 +93,25 @@ app.get('/profile', async (req, res) => {
     console.log(err);
   }
 });
+// fotos ophalen uit de database
+// router.get('/users', async (req, res) => {
+//   try {
+//     const matchedUser = await db.collection('users').findOne({
+//       name: 'Collin'
+//     });
+//     console.log(matchedUser);
+//     if (!matchedUser || !matchedUser.img) {
+//       throw new Error();
+//     }
+//     res.set('Content-Type', 'image/png');
+//     res.status(200).send(matchedUser.img);
+//     // stuurt een 404 error als de foto niet bestaat in de database
+//   } catch (error) {
+//     console.log(error);
+//     res.send(error);
+//   }
+// });
+
 
 app.get('/*', (req, res) => {
   res.status(404).render('error');
